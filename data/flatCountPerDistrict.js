f7ba3fffdadf220439737906bed158f9a25bcdcb
Saikek <saikek16@mgmail.com>
2020-10-20 20:16:55 +0200

    init commit of all resources

---
+++ data/flatCountPerDistrict.js
@@ -0,0 +1,33 @@
+//StackedLineChart colory thing:
+var flatCountPerDistrictData = [{
+
+    name: '1+kk',
+    data:
+        [29, 57, 51, 101, 111, 28, 26, 51, 85, 90, 7]
+}, {
+    name: '1+1',
+    data:
+        [10, 2, 8, 16, 10, 3, 4, 16, 10, 21, null]
+},
+{
+    name: '2+kk',
+    data:
+        [65, 58, 90, 218, 293, 50, 36, 108, 179, 133, 12]
+},
+{
+    name: '2+1',
+    data:
+        [23, 18, 35, 53, 28, 24, 17, 14, 26, 76, 1]
+},
+{
+    name: '3+kk',
+    data:
+        [69, 55, 84, 177, 212, 82, 19, 131, 148, 125, 12]
+},
+{
+    name: '3+1',
+    data:
+        [47, 40, 19, 86, 77, 42, 15, 39, 41, 59, 2]
+}];
+
+var flatCountPerDistrictLabels = ['Prague 1', 'Prague 2', 'Prague 3', 'Prague 4', 'Prague 5', 'Prague 6', 'Prague 7', 'Prague 8', 'Prague 9', 'Prague 10', 'Other'];
